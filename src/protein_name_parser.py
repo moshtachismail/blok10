fcf53a9a6be0129baba4da927f93122b99d45a98
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2022-09-19 21:05:26 +0200

    Update protein_name_parser.py

--- src/protein_name_parser.py
+++ src/protein_name_parser.py
@@ -4,36 +4,34 @@ This file parses the gennames given in a .txt file and writes
 the gennames to a new file
 """
 
-def main(chr_bestand, chr_output):
-    lijst_zonder_duplicates = file_reader_splitter(chr_bestand)
-    file_writer(chr_output, lijst_zonder_duplicates)
+def main(chr_file, chr_output):
+    list_without_duplicates = file_reader_splitter(chr_bestand)
+    file_writer(chr_output, list_without_duplicates)
 
-def file_reader_splitter(chr_bestand):
+def file_reader_splitter(chr_file):
     """
     This file parses the file with the gene names and also removes the
     duplicates
     :param chr_bestand: This is the name of the file which needs to be read
     :type chr_bestand: str
-    :return: lijst_zonder_duplicates
+    :return: lijst_without_duplicates
     :rtype: list
     """
-    chr_lezen = open(chr_bestand, 'r')
-    lijst_met_duplicates = []
-    lijst_zonder_duplicates = []
-    teller = 0
-    for x in chr_lezen:
-        if x.startswith('>'):
-            teller = teller + 1
+    chr_read = open(chr_file, 'r')
+    list_with_duplicates = []
+    list_without_duplicates = []
+    for x in chr_read:
+        if x.startswith('>'): # 
             line = x.split("|") 
-            lijn = line[2].split()
-            positie_gennaam = len(lijn) - 3 # de vaste positie van het gennaam is van de laatste positie in de zin 3 naar links
-            gennaam = lijn[positie_gennaam]
-            gen = gennaam.split("=") # in de gennaam zat er de = voor dus deze moet weg
-            lijst_met_duplicates.append(gen[1])
-    lijst_zonder_duplicates = list(set(lijst_met_duplicates)) # door het gebruik van een set worden alle duplicaten verwijdert
-    return lijst_zonder_duplicates
+            line2 = line[2].split()
+            position_genename = len(line2) - 3 # The recurring position of the name of the gene is 3 to the left from the back
+            genename = line2[position_genename]
+            gene = genename.split("=") # the gene name contains a "=" so this needs to be removed 
+            list_with_duplicates.append(gene[1])
+    list_without_duplicates = list(set(list_with_duplicates)) # the duplicates will be removed by using a set
+    return list_without_duplicates
 
-def file_writer(chr_output, lijst_zonder_duplicates):
+def file_writer(chr_output, list_without_duplicates):
     """
     :param chr_output: This is the name of the file which needs to be created
     :type chr_output: str
@@ -43,9 +41,9 @@ def file_writer(chr_output, lijst_zonder_duplicates):
     :rtype: N/A
     """
     new_file = open(chr_output, "w")
-    for x in lijst_zonder_duplicates:
+    for x in list_without_duplicates:
         new_file.write(x)
-        new_file.write("\n") # voegt enters toe aan het nieuwe bestand want anders worden alle string achter elkaar geplakt
+        new_file.write("\n") # the \n will make sure that there are enters after the strings
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
