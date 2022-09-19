1226258683d01d357f2c21918442bc1bdfc768b9
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2022-09-19 21:17:59 +0200

    Update SNP_data.py

--- src/SNP_data.py
+++ src/SNP_data.py
@@ -26,7 +26,7 @@ class Logger(object):
 
 
 def main(filename_in, filename_out):
-    sys.stdout = Logger("console_log.txt")
+    sys.stdout = Logger("console_log.txt") # this will create a log 
     start_time = datetime.datetime.now()
     proteins = get_proteins(filename_in)
     all_snps = get_data(proteins)
@@ -41,10 +41,19 @@ def main(filename_in, filename_out):
 
 
 def get_proteins(filename_in):
+    """
+    This Function makes sure that all proteins are saved in a list
+    and returned for later use
+    :parameter: filename_in: This the name of a file
+    :type: String
+    :return:
+    :proteins: A list with all protein names
+    :type: list
+    """
     proteins = []
     with open(filename_in) as file:
         for line in file:
-            if line != '':
+            if line != '': # if the line is not empty then the proteins will be appended to the list
                 proteins.append(line)
     print(len(proteins))
     return proteins
@@ -56,23 +65,23 @@ def get_data(proteins):
     found.
     """
     print("Searching for SNPs...")
-    all_snps = []
+    all_snps = [] # a list where all SNPs will be stored
     for protein in proteins:
         print(
             f"Current protein: {protein}. Index: {proteins.index(protein)}")
         try:
-            Entrez.email = ""
-            handle = Entrez.esearch(db="SNP", term=protein)
+            Entrez.email = "" 
+            handle = Entrez.esearch(db="SNP", term=protein) # this is a search function where it will be searching for SNPs corresponding to the gene name of the protein 
             record = Entrez.read(handle)
             count = int(record["Count"])
-            retmax = 10000
+            retmax = 10000 # this cap is determined, because if the cap is overriden then NCBI will deny access
             retstart = 0
             while retstart < count:
                 handle = Entrez.esearch(db="SNP", term=protein,
                                         retmax=retmax,
                                         retstart=retstart)
                 record = Entrez.read(handle)
-                id_list = record["IdList"]
+                id_list = record["IdList"] # this will get all IDs of the SNPs of the protein/gene
                 handle = Entrez.efetch(db="SNP", id=id_list,
                                        rettype="xml",
                                        retmax=retmax)
@@ -80,13 +89,13 @@ def get_data(proteins):
                     snp_data = handle.read()
                 except http.client.IncompleteRead as ir:
                     snp_data = ir.partial
-                snp_data = snp_data.decode("utf-8")
+                snp_data = snp_data.decode("utf-8") # this is necessary to read the file
                 snp_data = snp_data.split("\n")
                 del snp_data[0]
                 del snp_data[-1]
                 print(f"{len(snp_data)} SNPs were found.")
                 all_snps = filter_snp_data(snp_data, all_snps)
-                retstart += retmax
+                retstart += retmax # this will make sure that if the cap was overriden that the rest of the SNPs will be looked for
         except ValueError as ve:
             print(f"ValueError: {ve.__traceback__}")
         except IndexError as ie:
@@ -140,9 +149,9 @@ def write_to_file(file_lines, filename_out):
                    "\tVariation\tChromosome_Position\n")
         for line in file_lines:
             try:
-                if line[1] == "pathogenic" and line[4] == "snv":
+                if line[1] == "pathogenic" and line[4] == "snv": # this will make sure that only SNVs and pathogenic SNPs are stored to be written in a new file
                     new_line = f"{line[0]}\t{line[1]}\t{line[2]}\t{line[3]}\t" \
-                               f"{line[4]}\t{line[5]}\n"
+                               f"{line[4]}\t{line[5]}\n" # this makes sure that every bit of information is tab delimited so it can be stored as a TSV
                     file.write(new_line)
                     print(
                         f"Writing SNP: {line[0]}. Index: {file_lines.index(line)}")
